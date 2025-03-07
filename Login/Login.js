ac91b07c4c29d6b485168511aa9218dd855a68bc
MadMax-100 <max816940@gmail.com>
2025-03-07 16:33:49 +0000

    First Update

--- Login/Login.js
+++ Login/Login.js
@@ -1,3 +1,24 @@
+//Switch Between Login Page and Register Page
+const signInButton = document.getElementById("signInButton")
+const signUpButton = document.getElementById("signUpButton")
+
+const signInForm = document.getElementById("signIn")
+const signUpForm = document.getElementById("signUp")
+
+signUpButton.addEventListener('click', function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+
+})
+
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+
+})
+
+
+
 document.getElementById("loginbtn").addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -18,3 +39,6 @@ document.getElementById("loginbtn").addEventListener("click", function(event) {
         message.innerText = "Invalid email or password. Try again!";
     }
 });
+
+
+  
